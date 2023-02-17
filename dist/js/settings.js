9641de45233cbfce025484d1dd44ee9fd82bd566
Pawel Domagala <domagalapawel11@gmail.com>
2023-02-17 19:15:18 +0100

    Add booking page [WIP]

--- dist/js/settings.js
+++ dist/js/settings.js
@@ -2,15 +2,18 @@ export const select = {
 	templateOf: {
 		menuProduct: '#template-menu-product',
 		cartProduct: '#template-cart-product',
+		bookingWidget: '#template-booking-widget'
 	},
 	containerOf: {
 		menu: '#product-list',
 		cart: '#cart',
+		pages: '#pages',
+		booking: '.booking-wrapper'
 	},
 	all: {
 		menuProducts: '#product-list > .product',
 		menuProductsActive: '#product-list > .product.active',
-		formInputs: 'input, select',
+		formInputs: 'input, select'
 	},
 	menuProduct: {
 		clickable: '.product__header',
@@ -18,14 +21,31 @@ export const select = {
 		priceElem: '.product__total-price .price',
 		imageWrapper: '.product__images',
 		amountWidget: '.widget-amount',
-		cartButton: '[href="#add-to-cart"]',
+		cartButton: '[href="#add-to-cart"]'
 	},
 	widgets: {
 		amount: {
 			input: 'input.amount',
 			linkDecrease: 'a[href="#less"]',
-			linkIncrease: 'a[href="#more"]',
+			linkIncrease: 'a[href="#more"]'
 		},
+		datePicker: {
+			wrapper: '.date-picker',
+			input: `input[name="date"]`
+		},
+		hourPicker: {
+			wrapper: '.hour-picker',
+			input: 'input[type="range"]',
+			output: '.output'
+		}
+	},
+	booking: {
+		peopleAmount: '.people-amount',
+		hoursAmount: '.hours-amount',
+		tables: '.floor-plan .table'
+	},
+	nav: {
+		links: '.main-nav a'
 	},
 	cart: {
 		productList: '.cart__order-summary',
@@ -37,23 +57,33 @@ export const select = {
 		form: '.cart__order',
 		formSubmit: '.cart__order [type="submit"]',
 		phone: '[name="phone"]',
-		address: '[name="address"]',
+		address: '[name="address"]'
 	},
 	cartProduct: {
 		amountWidget: '.widget-amount',
 		price: '.cart__product-price',
 		edit: '[href="#edit"]',
-		remove: '[href="#remove"]',
+		remove: '[href="#remove"]'
 	}
 };
 
 export const classNames = {
 	menuProduct: {
 		wrapperActive: 'active',
-		imageVisible: 'active',
+		imageVisible: 'active'
 	},
 	cart: {
-		wrapperActive: 'active',
+		wrapperActive: 'active'
+	},
+	booking: {
+		loading: 'loading',
+		tableBooked: 'booked'
+	},
+	nav: {
+		active: 'active'
+	},
+	pages: {
+		active: 'active'
 	}
 };
 
@@ -61,19 +91,36 @@ export const settings = {
 	amountWidget: {
 		defaultValue: 1,
 		defaultMin: 1,
-		defaultMax: 9,
+		defaultMax: 9
 	},
 	cart: {
-		defaultDeliveryFee: 20,
+		defaultDeliveryFee: 20
+	},
+	hours: {
+		open: 12,
+		close: 24
+	},
+	datePicker: {
+		maxDaysInFuture: 14
+	},
+	booking: {
+		tableIdAttribute: 'data-table'
 	},
 	db: {
 		url: '//localhost:3131',
 		products: 'products',
 		orders: 'orders',
+		bookings: 'bookings',
+		events: 'events',
+		dateStartParamKey: 'date_gte',
+		dateEndParamKey: 'date_lte',
+		notRepeatParam: 'repeat=false',
+		repeatParam: 'repeat_ne=false'
 	}
 };
 
 export const templates = {
 	menuProduct: Handlebars.compile(document.querySelector(select.templateOf.menuProduct).innerHTML),
-	cartProduct: Handlebars.compile(document.querySelector(select.templateOf.cartProduct).innerHTML)
+	cartProduct: Handlebars.compile(document.querySelector(select.templateOf.cartProduct).innerHTML),
+	bookingWidget: Handlebars.compile(document.querySelector(select.templateOf.bookingWidget).innerHTML)
 };
--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,60 @@
-import {settings, select} from './settings.js';
+import {settings, select, classNames} from './settings.js';
 import Product from './components/Product.js';
 import Cart from './components/Cart.js';
+import Booking from './components/Booking.js';
 
 const app = {
+	initPages(){
+		const thisApp = this;
+
+		thisApp.pages = document.querySelector(select.containerOf.pages).children;
+		thisApp.navLinks = document.querySelectorAll(select.nav.links);
+
+		const idFromHash = window.location.hash.replace('#/', '');
+		let pageMatchingHash = thisApp.pages[0].id;
+
+		for(let page of thisApp.pages){
+			if(page.id == idFromHash){
+				pageMatchingHash = page.id;
+				break;
+			}
+		}
+
+		thisApp.activatePage(pageMatchingHash);
+
+		for(let link of thisApp.navLinks){
+			link.addEventListener('click', function(event){
+				event.preventDefault();
+				const clickedElement = this;
+				
+				// get page id from href attribute
+				const id = clickedElement.getAttribute('href').replace('#', '');
+
+				// run activatePage with that id
+				thisApp.activatePage(id);
+
+				// change URL hash
+				window.location.hash = '#/' + id;
+			});
+		}
+	},
+	activatePage: function(pageId){
+		const thisApp = this;
+
+		// add 'active' class to matching pages, remove from non-matching ones
+		for(let page of thisApp.pages)
+			page.classList.toggle(classNames.pages.active, page.id == pageId);
+		
+		// add 'active' class to matching links, remove from non-matching ones
+		for(let link of thisApp.navLinks)
+			link.classList.toggle(classNames.nav.active, link.getAttribute('href') == '#' + pageId);
+	},
+	initBooking(){
+		const thisApp = this;
+
+		thisApp.bookingPage = document.querySelector(select.containerOf.booking);
+		thisApp.booking = new Booking(thisApp.bookingPage);
+	},
 	initMenu(){
 		const thisApp = this;
 
@@ -41,9 +93,11 @@ const app = {
 	init: function(){
 		const thisApp = this;
 
+		thisApp.initPages();
 		thisApp.initData();
 		thisApp.initMenu();
 		thisApp.initCart();
+		thisApp.initBooking();
 	}
 };
 
