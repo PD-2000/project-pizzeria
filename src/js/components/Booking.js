13f6108b2c309ba51d4b31b6cd9a675b6a52943f
Pawel Domagala <domagalapawel11@gmail.com>
2023-02-19 20:00:02 +0100

    Add datePicker and hourPicker [WIP]

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -1,6 +1,8 @@
 import {templates, select} from '../settings.js';
 import utils from '../utils.js';
 import AmountWidget from './AmountWidget.js';
+import DatePicker from './DatePicker.js';
+import HourPicker from './HourPicker.js';
 
 class Booking{
 	constructor(element){
@@ -14,6 +16,7 @@ class Booking{
 
 		const generatedHTML = templates.bookingWidget();
 		thisBooking.element = utils.createDOMFromHTML(generatedHTML);
+
 		const bookingContainer = document.querySelector(select.containerOf.booking);
 		bookingContainer.appendChild(thisBooking.element).innerHTML;
 
@@ -21,19 +24,28 @@ class Booking{
 		thisBooking.dom.wrapper = element;
 		thisBooking.dom.peopleAmount = element.querySelector(select.booking.peopleAmount);
 		thisBooking.dom.hoursAmount = element.querySelector(select.booking.hoursAmount);
-
+		thisBooking.dom.datePicker = element.querySelector(select.widgets.datePicker.wrapper);
+		thisBooking.dom.hourPicker = element.querySelector(select.widgets.hourPicker.wrapper);
 	}
 	initWidgets(){
 		const thisBooking = this;
 
 		thisBooking.peopleAmount = new AmountWidget(thisBooking.dom.peopleAmount);
 		thisBooking.hoursAmount = new AmountWidget(thisBooking.dom.hoursAmount);
+		thisBooking.datePicker = new DatePicker(thisBooking.dom.datePicker);
+		thisBooking.hourPicker = new HourPicker(thisBooking.dom.hourPicker);
 
 		thisBooking.dom.peopleAmount.addEventListener('updated', function(){
 			thisBooking.amount = thisBooking.amountWidget;
 		});
 		thisBooking.dom.hoursAmount.addEventListener('updated', function(){
 			thisBooking.amount = thisBooking.amountWidget;
+		});
+		thisBooking.dom.datePicker.addEventListener('updated', function(){
+			thisBooking.element = thisBooking.datePicker;
+		});
+		thisBooking.dom.hourPicker.addEventListener('updated', function(){
+			thisBooking.element = thisBooking.hourPicker;
 		});
 	}
 }
